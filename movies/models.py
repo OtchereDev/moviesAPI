0c7dad0cb642500fbc88790d684437fd7faacfff
Oliver Otchere <oliverotchere4@gmail.com>
2020-12-17 21:54:25 +0000

    models added

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from .validators import validate_file_extension
+
+
+class Movie(models.Model):
+    title=models.CharField(max_length=500)
+    category=models.ForeignKey('category',on_delete=models.CASCADE)
+    description = models.TextField()
+    directors=models.CharField(max_length=225)
+    release_date=models.DateField()
+    movie_file=models.FileField(validators=[validate_file_extension,])
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=225)
