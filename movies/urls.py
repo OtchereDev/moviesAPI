7d6c32e639ea9c4e015a08ea5bd14c76f67daecc
Oliver Otchere <oliverotchere4@gmail.com>
2020-12-22 16:34:44 +0000

    list create view added

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,10 @@
+from django.urls import path
+
+from .views import MovieListCreateView
+
+
 app_name='movies_api'
 
 urlpatterns = [
-    
+    path('',MovieListCreateView.as_view()),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+
+from .models import Movie
+from .serializer import MovieSerializer,MovieCatSerializer
+
+class MovieListCreateView(ListCreateAPIView):
+    serializer_class=MovieSerializer
+    queryset=Movie.objects.all()
+
+
+
